5f2786c80fd692663cdab140f2ac9b8fe39d92c7
Ger Hobbelt <ger@hobbelt.com>
2018-09-30 10:15:12 +0200

    whitespace police raid

--- unpacked/longdiv.js
+++ unpacked/longdiv.js
@@ -27,60 +27,56 @@
  */
 MathJax.Extension["TeX/longdiv"] = {
     version: "0.1",
-            padtol: function (s, l) {
-            var v = "";
-            for (var tmp = s; tmp < l; tmp++) {
-                v = v + "\\?";
-            }
-            return v;
-        },
 
-        SimpleLongDiv: function (istr, jstr) {
-            il = istr.length;
-            jl = jstr.length;
+    padtol: function (s, l) {
+        var v = "";
+        for (var tmp = s; tmp < l; tmp++) {
+            v = v + "\\?";
+        }
+        return v;
+    },
 
-            j = parseInt(jstr, 10);
+    SimpleLongDiv: function (istr, jstr) {
+        il = istr.length;
+        jl = jstr.length;
 
-            ia = istr.substr(0, jl);
+        j = parseInt(jstr, 10);
 
+        ia = istr.substr(0, jl);
 
-            if (parseInt(ia, 10) < j) {
-                jl = jl + 1;
-            }
-
-            var jpadl = jstr.length + 1;
-
-            ans = "";
-            work = "";
-            work = jstr + "& \\hspace{-0.5em} \\enclose{longdiv}{" + istr + "}";
-            pad = "";
-
-
-            var ansline = "";
-            rstr = istr.substr(0, jl);
-
-
-            for (var k = jl; k <= il; k++) {
-                r = parseInt(rstr, 10);
-                a1 = Math.floor(r / j);
-                ans = ans + a1;
-                var jtimes = j * a1;
-                rstr = (r - j * a1) + "";
-                work = work + "\\\\\n&" + pad + "\\underline{" + this.padtol((jtimes + "").length, (k == jl ? jl : jpadl)) + jtimes + "}";
-                work = work + "\\\\\n&" + pad + this.padtol(rstr.length, (k == jl ? jl : jpadl)) + rstr + istr.substr(k, 1);
-                rstr = rstr + istr.substr(k, 1);
-                pad = ((k == jl && jl < jpadl) ? "" : "\\?") + pad;
-            }
-
-            var anspad = "";
-            for (var tmp = 0; tmp < jl - 1; tmp++) {
-                anspad = "\\?" + anspad;
-            }
-
-            return "\n\\def\\?{\\phantom{0}}\n\\begin{array}{r@{}l}\n&" + anspad + ans + "\\\\" + ansline + "\n" + work + "\n\\end{array}\n";
+        if (parseInt(ia, 10) < j) {
+            jl = jl + 1;
         }
 
+        var jpadl = jstr.length + 1;
 
+        ans = "";
+        work = "";
+        work = jstr + "& \\hspace{-0.5em} \\enclose{longdiv}{" + istr + "}";
+        pad = "";
+
+        var ansline = "";
+        rstr = istr.substr(0, jl);
+
+        for (var k = jl; k <= il; k++) {
+            r = parseInt(rstr, 10);
+            a1 = Math.floor(r / j);
+            ans = ans + a1;
+            var jtimes = j * a1;
+            rstr = (r - j * a1) + "";
+            work = work + "\\\\\n&" + pad + "\\underline{" + this.padtol((jtimes + "").length, (k == jl ? jl : jpadl)) + jtimes + "}";
+            work = work + "\\\\\n&" + pad + this.padtol(rstr.length, (k == jl ? jl : jpadl)) + rstr + istr.substr(k, 1);
+            rstr = rstr + istr.substr(k, 1);
+            pad = ((k == jl && jl < jpadl) ? "" : "\\?") + pad;
+        }
+
+        var anspad = "";
+        for (var tmp = 0; tmp < jl - 1; tmp++) {
+            anspad = "\\?" + anspad;
+        }
+
+        return "\n\\def\\?{\\phantom{0}}\n\\begin{array}{r@{}l}\n&" + anspad + ans + "\\\\" + ansline + "\n" + work + "\n\\end{array}\n";
+    }
 };
 
 MathJax.Hub.Register.StartupHook("TeX enclose Ready", function () {
@@ -108,8 +104,8 @@ MathJax.Hub.Register.StartupHook("TeX enclose Ready", function () {
     });
 
     MathJax.Hub.Startup.signal.Post("TeX longdiv Ready");
-
 });
+
 MathJax.Callback.Queue(
   ["Require",MathJax.Ajax,"[MathJax]/extensions/TeX/enclose.js"],
   ["loadComplete",MathJax.Ajax,"[Contrib]/longdiv/unpacked/longdiv.js"]
